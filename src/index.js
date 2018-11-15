88139595238438391c002e33a7131abee98105f8
slarribeau <slarribeau@gmail.com>
2018-11-15 09:27:02 -0800

    retrieve lat from browser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+/*const App = () => {
+
+   window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+   );
    return <div>Hi There!</div>
+}*/
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={lat:null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                console.log(position)
+                this.setState({lat: position.coords.latitude})
+            },
+            err => console.log(err)
+        );
+    }
+   render() {
+
+       return <div>Lattitude {this.state.lat}</div>;
+   }
 }
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
